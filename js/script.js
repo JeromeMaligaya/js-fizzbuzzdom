825c1748d1adde912f1118aa20f42f935648af3d
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-02 21:58:35 +0200

    fixed for list

--- js/script.js
+++ js/script.js
@@ -20,21 +20,29 @@ let list = '<ul>';
 console.log('list start', list);
     
 // 3.lavorazione dati
+// inizio ciclo for
 for (let i=1; i<= 100; i++){
+    // se multipli di 3 e 5
     if (i % 3 === 0 && i % 5 === 0){
     console.log('FizzBuzz')
-    list += `<li class="fizzbuzz">FizzBuzz<li>`
+    list += `<li class="fizzbuzz">FizzBuzz</li>`
+    // se multipli di 3
     }else if(i % 3 === 0){
         console.log('Fizz')
-        list += `<li class="fizz">Fizz<li>`
+        list += `<li class="fizz">Fizz</li>`
+    // se multipli di 5
     }else if(i % 5 === 0){
         console.log(`Buzz`)
-        list += `<li class="Buzz">Buzz<li>`
+        list += `<li class="Buzz">Buzz</li>`
+    // valore non multiplicabile
     }else{
         console.log(`${i}`)
-        list += `${i}`
+        list += `<li class="i-js">${i}</li>`
     }
     
 }
+// chiusura stringa
+
 
 // 4.generazione output
+
