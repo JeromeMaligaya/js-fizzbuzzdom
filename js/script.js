abc2dd7416b02e9906db60150b1fc204de2d6e5f
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-02 21:13:43 +0200

    recupero l'elemento dal dom

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 //Check link
 console.log('JS OK')
 
-// 1.recupero l'elemento dal Dom
+/*scaletta
+1.recupero dati
+2.creo costante e inizio la lavorazione dei dati
+3.apro e modello il ciclo for
+4.chiudo stringa
+5.stampo in console
 
+
+*/
+
+// 1.recupero l'elemento dal Dom
+const target = document.getElementById('target');
+console.log('target on index', target);
 
 //2.raccolta dati
+
     
 // 3.lavorazione dati
-    
+
 
 // 4.generazione output
